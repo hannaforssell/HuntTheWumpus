c684940dcb1e2a96901afdb831368e3926829d55
hannaforssell <forssellhanna@gmail.com>
2023-02-03 18:45:59 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/fed22d-js-grundkurs-2-wumpus-hannaforssell/',
+  base: '/HuntTheWumpus/',
   define: {
     'process.env': {},
   },
